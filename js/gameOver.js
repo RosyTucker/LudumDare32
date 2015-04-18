8ef6888535adfeb2b68baf414e32d5897391b641
Rosy Tucker <rtucker@thoughtworks.com>
2015-04-19 00:58:39 +0100

    Add Game over text

--- js/gameOver.js
+++ js/gameOver.js
@@ -1,4 +1,10 @@
 
+WebFontConfig = {
+    google: {
+        families: ['Revalia']
+    }
+
+};
 
 GameOverState = function(game){
     this.didWin = false;
@@ -6,14 +12,45 @@ GameOverState = function(game){
 };
 
 GameOverState.prototype.preload = function(){
+    this.game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
 };
 
 GameOverState.prototype.create = function(){
+    this.game.stage.setBackgroundColor('#B8D973ff');
     console.log('game over: ' + this.didWin);
+    this.createText();
 };
 
 GameOverState.prototype.update = function (){
+    if(this.game.input.activePointer.isDown){
+        this.game.state.start('inGame');
+    }
 };
 
 GameOverState.prototype.render = function() {
+};
+
+GameOverState.prototype.createText = function () {
+    var result = 'Lost :(';
+    if(this.didWin) result = 'Won :D';
+    var text = this.game.add.text(0,0, "Game Over \n You " + result + '\n\n Click to play again!');
+    text.anchor.setTo(0.5);
+
+    text.font = 'Revalia';
+    text.fontSize = 60;
+
+    text.align = 'center';
+    text.fill = '#FFFFFF';
+    text.stroke = '#000000';
+    text.strokeThickness = 2;
+    text.setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
+
+    text.inputEnabled = true;
+    text.events.onInputOver.add(function(){
+        text.fill = '#6A665A';
+    }, this);
+    text.events.onInputOut.add(function(){
+        text.fill = '#FFFFFF';
+    }, this);
+
 };
