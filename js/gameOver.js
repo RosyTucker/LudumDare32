5c6e770a60d5e546c488825e5212eb0cb947ba77
Rosy Tucker <rtucker@thoughtworks.com>
2015-04-19 00:32:04 +0100

    Add game over state

---
+++ js/gameOver.js
@@ -0,0 +1,16 @@
+GameOverState = function(game){
+    this.game = game;
+};
+
+GameOverState.prototype.preload = function(){
+   console.log('game over')
+};
+
+GameOverState.prototype.create = function(){
+};
+
+GameOverState.prototype.update = function (){
+};
+
+GameOverState.prototype.render = function() {
+};
--- js/preloader.js
+++ js/preloader.js
@@ -5,8 +5,11 @@ function Preloader(game)
 
 Preloader.prototype.preload = function() {
     var inGameState = new InGameState(this.game);
+    var gameOverState = new GameOverState(this.game);
     inGameState.preload();
+    gameOverState.preload();
     this.game.state.add('inGame', inGameState);
+    this.game.state.add('gameOver', gameOverState);
 };
 
 Preloader.prototype.create = function() {
