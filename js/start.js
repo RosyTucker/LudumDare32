9aff1f737cb1eb54b25bcc2706c21ca1c234461b
Rosy Tucker <rtucker@thoughtworks.com>
2015-04-19 18:11:30 +0100

    Fix Typo

--- js/start.js
+++ js/start.js
@@ -1,5 +1,5 @@
 $(function() {
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game0canvas-container', null, false, false);
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-canvas-container', null, false, false);
     var preloader = new Preloader(game);
     game.state.add('preloader', preloader);
     game.state.start('preloader');
